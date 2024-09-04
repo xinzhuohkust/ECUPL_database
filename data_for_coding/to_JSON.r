dc11f9d51008492e4b20e33c4feda9d09a451973
Hsincho <99251348+xinzhuohkust@users.noreply.github.com>
2024-09-04 17:59:50 +0800

    first commit

---
+++ data_for_coding/to_JSON.r
@@ -0,0 +1,19 @@
+pacman::p_load(tidyverse, rio, jsonlite)
+
+rio::import("data_for_coding/基层治理中文文献数据.xlsx", setclass = "tibble") %>%
+    mutate(id = row_number()) %>%
+    select(
+        title = `Title-题名`,
+        author = "Author-作者",
+        source = "Source-文献来源",
+        year = "Year-年",
+        keywords = "关键词",
+        summary = "摘要",
+        institution = "机构"
+    ) %>%
+    mutate(
+        across(everything(), \(x) str_trim(x) |> str_squish()),
+        across(everything(), \(x) replace_na(x, "未知")),
+    ) %>%
+    sample_frac(size = 1) %>% 
+    rio::export("data_for_coding/基层治理中文文献数据.json", json_pretty = TRUE)
