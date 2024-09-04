70e01bd1882b62857973f23ecce93ab3fd7b1b68
Hsincho <99251348+xinzhuohkust@users.noreply.github.com>
2024-09-04 21:30:19 +0800

    Wed, Sep  4, 2024  9:30:19 PM

--- data_for_coding/to_JSON.r
+++ data_for_coding/to_JSON.r
@@ -1,7 +1,7 @@
 pacman::p_load(tidyverse, rio, jsonlite)
 
-rio::import("data_for_coding/基层治理中文文献数据.xlsx", setclass = "tibble") %>%
-    mutate(id = row_number()) %>%
+rio::import("F:/OneDrive - HKUST Connect/Phd/网站范例/华东政法数据库项目网站/data_for_coding/基层治理中文文献数据.xlsx", setclass = "tibble") %>%
+    #mutate(id = row_number()) %>%
     select(
         title = `Title-题名`,
         author = "Author-作者",
@@ -17,3 +17,16 @@ rio::import("data_for_coding/基层治理中文文献数据.xlsx", setclass = "t
     ) %>%
     sample_frac(size = 1) %>% 
     rio::export("docs/databases/基层治理文献摘编数据库/基层治理中文文献数据.json")
+
+rio::import("F:/OneDrive - HKUST Connect/Phd/网站范例/华东政法数据库项目网站/data_for_coding/政治学名篇摘编汇总.xlsx", setclass = "tibble") %>%
+    select(
+        topic = "主题",
+        summary = "经典摘录",
+        source = "出处"
+    ) %>%
+    mutate(
+        across(everything(), \(x) str_trim(x) |> str_squish()),
+        across(everything(), \(x) replace_na(x, "未知")),
+    ) %>%
+    sample_frac(size = 1) %>% 
+    rio::export("docs/databases/基层治理文献摘编数据库/政治学名篇摘编汇总.json")
